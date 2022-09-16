2e7850d3e55ea875c80d514b9522f7ee1612d040
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-09-16 13:06:38 +0200

    add stuff (initial)

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-title>gayguessr</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: () => ({ }),
+}
+</script>
