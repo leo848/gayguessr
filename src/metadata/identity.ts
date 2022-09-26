9fe09520f20ca79f70deaee3f2de194aaa5b2a71
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-09-26 17:34:49 +0200

    load identity

--- src/metadata/identity.ts
+++ src/metadata/identity.ts
@@ -1,10 +1,26 @@
 import { Flag } from '../flags/flag';
 import { flagPresets } from '../flags/flagPresets';
+import { parse } from 'yaml';
 
-type Identity = {
-	  name: string;
+type YamlIdentity = {
+	  title: string;
 	  aliases: string[];
 	  description: string;
+	  categories: string[];
+};
+
+
+type Identity = YamlIdentity & {
 	  flag: Flag;
 	  alternativeFlags: Flag[];
 };
+
+export async function loadIdentity(key: string): Promise<Identity> {
+	let fetchedRawYaml = await fetch(process.env.BASE_URL + '/metadata/' + key + '.yaml');
+	let yamlIdentity = parse(await fetchedRawYaml.text()) as YamlIdentity;
+	return {
+		...yamlIdentity,
+		flag: flagPresets[yamlIdentity.title],
+		alternativeFlags: [] // TODO: load alternative flags
+	}
+}
