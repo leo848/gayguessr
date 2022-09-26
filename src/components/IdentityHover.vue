835a25320aa683a37d9a0c72c92828a0d27727fc
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-09-26 17:48:52 +0200

    Add IdentityHover component (see #9)

---
+++ src/components/IdentityHover.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        v-bind="attrs"
+        v-on="on"
+        color="primary"
+        dark
+        class="mr-2"
+      >
+        Hover Me
+      </v-btn>
+      <v-card>
+        <v-card-title>{{ identity }}</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, earum! Velit provident quos distinctio natus perferendis ratione, consequatur sequi aut neque, iste modi ad tenetur harum reiciendis quisquam officiis suscipit.
+        </v-card-text>
+      </v-card>
+    </template>
+  </v-menu>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'IdentityHover',
+  props: {
+    identity: {
+      type: String,
+      required: true
+    }
+  },
+  created() {
+    console.log(this.identity)
+  }
+}
+</script>
