1d490b196fe1fa4a936c9e3754f5e7db8e413a74
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-02 19:23:12 +0200

    Save games to local storage

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,23 +1,58 @@
 <template>
   <v-container>
     <GameComponent class="mt-2" @done="showAlert" />
+    <v-dialog v-model="gameDialog" persistent>
+      <v-card v-if="game">
+        <v-card-title>Well done!</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="2">
+            <v-progress-circular
+              :model-value="100 * game.playedFlags.filter(f => f.correct).length / game.playedFlags.length"
+              color="green"
+              size="64"
+              width="8"
+              />
+            </v-col>
+            <v-col>
+              <p class="text-h5">
+                Correct: {{ game.playedFlags.filter(f => f.correct).length }} out of {{ game.playedFlags.length }}
+              </p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn @click="close">Return to dashboard</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script lang="ts">
 import GameComponent from '../components/Game.vue'
 
-import type { Game } from '../types/game'
+import type { Game } from '../storage/types'
+import { loadGames, saveGame } from '../storage/storage'
 
 export default {
   name: 'Game',
   components: { GameComponent },
   data: () => ({
-    //
+    gameDialog: false,
+    game: null as Game | null,
   }),
   methods: {
     showAlert(game: Game) {
-      alert(`You got ${game.playedFlags.filter(o => o.correct).length} out of ${game.playedFlags.length} correct!`);
+      this.game = game;
+      this.gameDialog = true;
+    },
+    close() {
+      this.gameDialog = false;
+      saveGame(this.game);
+      this.game = null;
+      this.$router.push('/');
     },
   },
 }
