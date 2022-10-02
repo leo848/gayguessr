6afe0e878082be12c399f0ea09364fa2a3110531
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-02 16:50:59 +0200

    Add button to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container>
     <h1>Dashboard</h1>
+    <v-row>
+      <v-col cols="3">
+        <v-card to="./game">
+          <v-card-title>New Game</v-card-title>
+          <v-card-text>
+            <v-icon size="80">mdi-plus-circle</v-icon>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
