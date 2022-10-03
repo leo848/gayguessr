ef910390708eb484868fb50995543866e42ba44c
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-03 15:10:27 +0200

    outsource GamesList

---
+++ src/components/GamesList.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6" md="4" xl="3">
+      <v-card to="./game">
+        <v-card-title class="text-h4">New Game</v-card-title>
+        <v-card-text>
+          <v-icon size="80">mdi-plus-circle</v-icon>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col
+      v-for="game in games.slice().reverse().slice(0, 5)"
+      :key="game.id"
+      cols="12"
+      sm="6"
+      md="4"
+      xl="3"
+      >
+      <v-card height="100%">
+        <v-card-title class="text-h4">#{{ game.id }}</v-card-title>
+        <v-card-subtitle class="text-h6">
+          {{ formatDate(game.timeStarted) }} 
+        </v-card-subtitle>
+        <v-card-text>
+          <p>
+            <b>{{ game.playedFlags.length }}</b> flags
+            • <b>{{ game.playedFlags.filter(f => f.correct).length }}</b> 
+            <span class="text-success"> correct</span>
+            • <b>{{ game.playedFlags.filter(f => !f.correct).length }}</b> 
+            <span class="text-error"> wrong</span>
+          </p>
+          <p>
+
+          </p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+export default {
+  name: "GamesList",
+  props: {
+    games: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    formatDate(date: Date) {
+      return date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      });
+    }
+  }
+};
+</script>
+
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,43 +1,7 @@
 <template>
   <v-container>
     <h1 class="text-h3 mb-4">Dashboard</h1>
-    <v-row>
-      <v-col cols="12" sm="6" md="4" xl="3">
-        <v-card to="./game">
-          <v-card-title class="text-h4">New Game</v-card-title>
-          <v-card-text>
-            <v-icon size="80">mdi-plus-circle</v-icon>
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col
-        v-for="game in games.slice().reverse().slice(0, 5)"
-        :key="game.id"
-        cols="12"
-        sm="6"
-        md="4"
-        xl="3"
-        >
-        <v-card height="100%">
-          <v-card-title class="text-h4">#{{ game.id }}</v-card-title>
-          <v-card-subtitle class="text-h6">
-            {{ formatDate(game.timeStarted) }} 
-          </v-card-subtitle>
-          <v-card-text>
-            <p>
-              <b>{{ game.playedFlags.length }}</b> flags
-              • <b>{{ game.playedFlags.filter(f => f.correct).length }}</b> 
-              <span class="text-success"> correct</span>
-              • <b>{{ game.playedFlags.filter(f => !f.correct).length }}</b> 
-              <span class="text-error"> wrong</span>
-            </p>
-            <p>
-
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+    <GamesList :games="games" />
     <h2 class="text-h4 mt-6">
       Flag of the day ({{ new Date().toLocaleDateString() }})
       <v-tooltip top max-width="300px">
@@ -60,17 +24,13 @@
 import { loadGames } from '../storage/storage'
 
 import FlagOfTheDay from '../components/FlagOfTheDay.vue'
+import GamesList from '../components/GamesList.vue'
 
 export default {
   name: 'Dashboard',
   data: () => ({
     games: loadGames(),
   }),
-  components: { FlagOfTheDay },
-  methods: {
-    formatDate(date: Date) {
-      return Intl.DateTimeFormat('en-US', { dateStyle: 'medium', timeStyle: 'short' }).format(date);
-    }
-  }
+  components: { FlagOfTheDay, GamesList },
 }
 </script>
