6c61b9012a0ba7e9076757282d80fd4a4cb8fb84
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-05 00:12:53 +0200

    Add InfoTooltip to uniformly display tooltips

---
+++ src/components/InfoTooltip.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-tooltip top :max-width="maxWidth">
+    <template v-slot:activator="{ props }">
+      <v-btn :icon="icon" variant="text" v-bind="props" />
+    </template>
+    <slot />
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'InfoTooltip',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    maxWidth: {
+      type: String,
+      default: '300px',
+    },
+    icon: {
+      type: String,
+      default: 'mdi-information',
+    },
+  },
+}
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,16 +9,11 @@
           <v-card-text>
             <h2 class="text-h4">
               Flag of the day ({{ new Date().toLocaleDateString() }})
-              <v-tooltip top max-width="300px">
-                <template v-slot:activator="{ props }">
-                  <v-btn icon="mdi-information" variant="text" v-bind="props" />
-                </template>
-                <span>
+              <InfoTooltip>
                   The flag of the day is calculated based on the hash of the current day
                   and is dependent on the amount of flags. This means that as more flags
                   get added, the flag of the day might change more than once per day.
-                </span>
-              </v-tooltip>
+              </InfoTooltip>
             </h2>
             <p class="mb-4">Click for more information.</p>
             <FlagOfTheDay width="70%" />
@@ -43,6 +38,7 @@ import { loadGames } from '../storage/storage'
 import type { Game } from '../storage/types'
 
 import FlagOfTheDay from '../components/FlagOfTheDay.vue'
+import InfoTooltip from '../components/InfoTooltip.vue'
 import GamesList from '../components/GamesList.vue'
 
 import { Duration } from 'luxon'
@@ -76,6 +72,6 @@ export default {
       return Duration.fromObject({ days, hours, minutes, seconds }).normalize().toHuman()
     },
   },
-  components: { FlagOfTheDay, GamesList },
+  components: { FlagOfTheDay, GamesList, InfoTooltip },
 }
 </script>
