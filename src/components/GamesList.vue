7661d6a97cfec1efb57ce6441cc347be0b080bae
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-06 13:50:10 +0200

    Game start dialog

--- src/components/GamesList.vue
+++ src/components/GamesList.vue
@@ -1,11 +1,14 @@
 <template>
   <v-row style="min-height: 100%">
     <v-col v-if="!noNewGame" cols="12" sm="6" md="4" xl="3">
-      <v-card to="./game">
+      <v-card @click="dialog = true">
         <v-card-title class="text-h4">New Game</v-card-title>
         <v-card-text>
           <v-icon size="80">mdi-plus-circle</v-icon>
         </v-card-text>
+        <v-dialog v-model="dialog" max-width="600px">
+          <NewGameDialog />
+        </v-dialog>
       </v-card>
     </v-col>
     <v-col
@@ -39,8 +42,11 @@
 </template>
 
 <script lang="ts">
+import NewGameDialog from './NewGameDialog.vue';
+
 export default {
   name: "GamesList",
+  components: { NewGameDialog },
   props: {
     games: {
       type: Array,
@@ -55,6 +61,9 @@ export default {
       default: false
     },
   },
+  data: () => ({
+    dialog: false
+  }),
   methods: {
     formatDate(date: Date) {
       return date.toLocaleDateString("en-US", {
---
+++ src/components/NewGameDialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h2 class="text-h4 mb-2">Start a new game</h2>
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" to="./game">Start</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NewGameDialog",
+}
+</script>
