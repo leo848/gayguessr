0adebbb1ac4520d0c2d1fc6685674055f77042be
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-10-07 22:45:23 +0200

    fix formatting

--- src/views/AllFlags.vue
+++ src/views/AllFlags.vue
@@ -1,60 +1,60 @@
 <template>
   <v-container>
     <v-row>
-        <v-col cols="10">
-          <v-text-field
-            class="mt-4"
-            v-model="search"
-            :error="error !== null"
-            :error-messages="error || undefined"
-            append-icon="mdi-magnify"
-            :label="regex ? 'Enter RegExp...' : 'Search...'"
-            clearable
-            single-line
-            hide-details
-            />
-        </v-col>
-        <v-col cols="2">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ props }">
-              <v-btn :icon="regex ? 'mdi-cursor-text' : 'mdi-regex'" class="mt-4" v-bind="props" @click="regex = !regex" />
-            </template>
-            <span>{{ regex ? "Use text mode" : "Use RegExp mode" }}</span>
-          </v-tooltip>
-        </v-col>
-        <v-col cols="12">
-          <p class="text-h5" v-if="flags.filter(test).length > 0">{{ search ? `${ flags.filter(test).length } flags found` : `Showing ${ flags.filter(test).length } flags` }}
-            <span v-if="search" class="text-disabled">{{ regex ? `matching ${ search }` : `containing ${ search }` }}</span>
-          </p>
-          <p class="text-h5" v-else>
-            No flags matched your search.<br/>
-            <span v-if="suggestion">
-              Did you mean: <i @click="search = suggestion" class="text-primary">{{ suggestion }}</i>
-            </span>
-          </p>
-        </v-col>
-        <v-col v-for="(name, index) in flags" :key="name + index" v-show="test(name)" cols="12" sm="6" lg="4">
-          <IdentityInfo :id="name" always-show>
-          <v-card>
-            <v-card-title v-html="highlight(name, search)" />
+      <v-col cols="10">
+        <v-text-field
+          class="mt-4"
+          v-model="search"
+          :error="error !== null"
+          :error-messages="error || undefined"
+          append-icon="mdi-magnify"
+          :label="regex ? 'Enter RegExp...' : 'Search...'"
+          clearable
+          single-line
+          hide-details
+          />
+      </v-col>
+      <v-col cols="2">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ props }">
+            <v-btn :icon="regex ? 'mdi-cursor-text' : 'mdi-regex'" class="mt-4" v-bind="props" @click="regex = !regex" />
+          </template>
+          <span>{{ regex ? "Use text mode" : "Use RegExp mode" }}</span>
+        </v-tooltip>
+      </v-col>
+      <v-col cols="12">
+        <p class="text-h5" v-if="flags.filter(test).length > 0">{{ search ? `${ flags.filter(test).length } flags found` : `Showing ${ flags.filter(test).length } flags` }}
+          <span v-if="search" class="text-disabled">{{ regex ? `matching ${ search }` : `containing ${ search }` }}</span>
+        </p>
+        <p class="text-h5" v-else>
+          No flags matched your search.<br/>
+          <span v-if="suggestion">
+            Did you mean: <i @click="search = suggestion" class="text-primary">{{ suggestion }}</i>
+          </span>
+        </p>
+      </v-col>
+      <v-col v-for="(name, index) in flags" :key="name + index" v-show="test(name)" cols="12" sm="6" lg="4">
+        <IdentityInfo :id="name" always-show>
+        <v-card>
+          <v-card-title v-html="highlight(name, search)" />
             <v-card-text>
               <Flag :preset="name" />
             </v-card-text>
           </v-card>
-          </IdentityInfo>
-        </v-col>
-        <v-col cols="12" sm="6" lg="4" v-if="flags.filter(test).length === 0">
-          <v-card color="error">
-            <v-card-text>
-              <p class="text-h3 mb-4">No flags found</p>
-              <p class="text-h5">
-                You want to suggest or add a flag or identity?
-                Open an <v-btn :href="'https://github.com/leo848/gayguessr/issues/new?assignees=&labels=flag-request&template=request-a-flag---identity.md&title=Add%20'+search" target="_blank" variant="tonal" size="large">issue</v-btn>
-                on <v-btn href="https://github.com/leo848/gayguessr/" variant="tonal">GitHub</v-btn>.
-              </p>
-            </v-card-text>
-          </v-card>
-        </v-col>
+        </IdentityInfo>
+      </v-col>
+      <v-col cols="12" sm="6" lg="4" v-if="flags.filter(test).length === 0">
+        <v-card color="error">
+          <v-card-text>
+            <p class="text-h3 mb-4">No flags found</p>
+            <p class="text-h5">
+              You want to suggest or add a flag or identity?
+              Open an <v-btn :href="'https://github.com/leo848/gayguessr/issues/new?assignees=&labels=flag-request&template=request-a-flag---identity.md&title=Add%20'+search" target="_blank" variant="tonal" size="large">issue</v-btn>
+              on <v-btn href="https://github.com/leo848/gayguessr/" variant="tonal">GitHub</v-btn>.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
